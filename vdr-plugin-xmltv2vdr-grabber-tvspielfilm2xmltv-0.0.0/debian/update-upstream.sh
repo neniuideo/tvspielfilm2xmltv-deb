029f7c08015fdfa90d7fb3e69d01cae926915c94
neniuideo <vdr@unterderbruecke.de>
2013-09-01 11:42:44 +0200

    Update update-upstream.sh

--- vdr-plugin-xmltv2vdr-grabber-tvspielfilm2xmltv-0.0.0/debian/update-upstream.sh
+++ vdr-plugin-xmltv2vdr-grabber-tvspielfilm2xmltv-0.0.0/debian/update-upstream.sh
@@ -39,7 +39,7 @@ case $1 in
     tmp="$(mktemp -d)"
     curdir="$(pwd)"
     cd "${tmp}"
-    git clone https://github.com/neniuideo/tvspielfilm2xmltv.git
+    git clone https://github.com/chriszero/tvspielfilm2xmltv.git
     cd "${tmp}/tvspielfilm2xmltv"
     upstream=$(git describe --tags --dirty | sed 's/^v//')
     cd "${curdir}"
